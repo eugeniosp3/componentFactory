d20a974f504b82bc1e991c693653683cf987902a
Gino P <64187887+eugeniosp3@users.noreply.github.com>
2023-07-28 22:21:28 -0400

    responsive

--- app/components/Accordion/Accordion.jsx
+++ app/components/Accordion/Accordion.jsx
@@ -5,18 +5,22 @@ import {FaAngleDown} from 'react-icons/fa'
 function Accordion({header, badge}) {
     const [open, setOpen] = React.useState(false)
   return (
-    <div className={`bg-white backdrop-blur-md  hover:ease-in-out hover:duration-600 ease-in-out duration-700 
-                    w-11/12  
-                    rounded-3xl 
+    <div className={`backdrop-blur-md flex flex-col items-center hover:ease-in-out hover:duration-600 ease-in-out duration-700 
+                    xxs:w-11/12 xs:w-11/12 sm:w-11/12 md:w-11/12 lg:w-1/2 xl:w-1/2 2xl:w-1/2
+                    xxs:bg-cyan-100 xs:bg-teal-200 sm:bg-purple-200 md:bg-blue-200 lg:bg-purple-200 xl:bg-yellow-100 2xl:bg-green-200
+                    rounded-xl 
                     ${open ? "h-96" : "h-16"} 
                     relative
                     mb-4
+   
                     `}
                     
                     >
-                        <div className="w-full h-16 bg-gray-100 hover:bg-gray-100 rounded-xl px-10 py-4
+                        <div className="h-16 bg-white hover:bg-gray-100 rounded-xl px-10 py-4
                          flex justify-start items-center duration-700 ease-out hover:drop-shadow-lg active:drop-shadow-none drop-shadow-sm
                          cursor-pointer relative
+                         mx-auto
+                         xxs:w-full xs:w-full sm:w-full md:w-full lg:w-full xl:w-full 2xl:w-full
                          "
                          onClick={() => setOpen(!open)}
                          >
@@ -25,42 +29,42 @@ function Accordion({header, badge}) {
                             <div className="flex items-center justify-center
                              border-solid border border-red-600 rounded-lg
                               bg-red-100/50 px-3 ml-4 h-6">
-                                <p className="text-red-600 font-regular text-sm">Risk</p>
+                                <p className="text-red-600 font-regular text-sm select-none">Risk</p>
                                 </div>
                         </div>
                         <p className="
                                     text-white text-3xl text-center
-                                    absolute right-12
+                                    absolute lg:right-12 right-8
                                     select-none
                                     ease-in-out duration-1000
                                     ">
                         <FaAngleDown 
-                        
                         className={open ? "transform rotate-180 duration-500 text-slate-800" : "transform rotate-0 duration-500 text-slate-800"}
                         
                         />
         </p>
         </div>
-        {open ? <div className="backdrop-blur-xl w-5/6 h-4/6
-         relative left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/4
+
+        {open ? <div className="backdrop-blur-xl w-5/6 h-4/6 p-6
+         relative left-max-content top-1/4 -translate-y-1/4
           rounded-lg overflow-y-scroll select-none cursor-text
-           ease-in-out duration-1000 ">
+           ease-in-out duration-1000 bg-red-100">
             <p className="ease-in-out duration-300 transition delay-1000 ">
-                <p className="text-gray-400 text-md mb-4">Balance Summary</p>
+                <p className="text-black/30 text-md mb-4">Balance Summary</p>
                 <ul>
-                    <li className="hover:text-gray-900 hover:font-semibold text-gray-800 cursor-default
+                    <li className=" text-gray-800 cursor-default
                      text-md mb-4 
                      flex items-center justify-start">Bank of America $12,000.00
                     <div className="flex items-center justify-center border-solid border
                      border-red-600 rounded-lg bg-red-100/50
                       px-3 ml-4 h-6"><p className="text-red-600 font-regular text-sm">Risk</p></div></li>
-                    <li className="hover:text-gray-900 hover:font-semibold text-gray-800 cursor-default
+                    <li className=" text-gray-800 cursor-default
                      text-md mb-4"
                      >Capital One $12.97</li>
-                    <li className="hover:text-gray-900 hover:font-semibold text-gray-800 cursor-default
+                    <li className=" text-gray-800 cursor-default
                      text-md mb-4"
                      >American Express $13.14</li>
-                    <li className="hover:text-gray-900 hover:font-semibold text-gray-800 cursor-default
+                    <li className=" text-gray-800 cursor-default
                      text-md mb-4 
                      flex items-center justify-start">Chase Bank $0.00
                     <div className="flex items-center justify-center border-solid border
--- app/page.js
+++ app/page.js
@@ -3,10 +3,24 @@ import Accordion from './components/Accordion/Accordion'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-around p-24">
+    <main className="flex flex-col items-center justify-around p-24 
+    xxs:bg-rose-100 xs:bg-rose-300 sm:bg-rose-500 md:bg-yellow-300 lg:bg-sky-300 xl:bg-purple-400 2xl:bg-green-500
+    w-screen
+    
+    ">
+      <div className="flex flex-col items-center justify-around w-screen">
      <Accordion />
+     </div>
+
+     <div className="flex flex-col items-center justify-around w-screen">
      <Accordion />
+     </div>
+
+
+     <div className="flex flex-col items-center justify-around w-screen">
      <Accordion />
+     </div>
+
 
     </main>
   )
