196b73e1f9f10d366c2297b3246fdfe51167cd18
Gino P <64187887+eugeniosp3@users.noreply.github.com>
2023-07-30 04:51:18 -0400

    fixed responsive stuff more

--- app/components/NavBar/NavBar.jsx
+++ app/components/NavBar/NavBar.jsx
@@ -28,26 +28,26 @@ const handleClickLink = (index) => {
   return (
     // holds outer div for menu
     <div
-      className="h-28 rounded-xl
-      xxs:w-11/12 xs:w-11/12 sm:w-11/12 md:w-11/12 lg:w-5/6 xl:w-3/5 2xl:w-1/2
+      className="h-28 xxs:max-md:h-96 rounded-xl
+      xxs:w-3/4 xs:w-2/4 sm:w-11/12 md:w-11/12 lg:w-5/6 xl:w-3/5 2xl:w-1/2
       xxs:bg-pink-300 xs:bg-slate-300 sm:bg-orange-300 md:bg-sky-300 lg:bg-stone-300 xl:bg-green-300 2xl:bg-teal-300
-      flex flex-col items-center justify-center ease-in-out duration-700 
+      flex flex-col items-center justify-center ease-in-out duration-700 cursor-pointer
       ">
         {/* holds menu items */}
-        <div className="gap-5 text-sm border-b-2 border-b-blue-100/50 bg-slate-800 border backdrop-blur-2xl shadow-md shadow-black/30 rounded-lg
-        flex items-center justify-center w-11/12 xxs:max-md:text-xs/[1em] xxs:max-md:flex-2 xxs:max-md:w-11/12 xxs:max-md: xxs:max-md:gap-4 xxs:max-md:px-2 md:whitespace-nowrap
+        <div className="gap-5 xxs:max-md:h-84 text-sm border-b-2 border-b-blue-100/50 bg-slate-800 border backdrop-blur-2xl shadow-md shadow-black/30 rounded-lg shadow-inner/30
+        flex xxs:max-sm:flex-col items-center justify-center w-11/12 xxs:max-lg:text-xs/[1em] xxs:max-sm:flex-2 xxs:max-sm:w-3/5  xxs:max-md:gap-1 xxs:max-md:px-2 xxs:max-lg:whitespace-nowrap
       
         ">
                 {links.map((link, index) => (
-        <div key={index} className="flex flex-col items-center justify-center"
+        <div key={index} className="flex flex-col xxs:max-smflex-row items-center xxs:max-smitems-center xxs:max-smw-full justify-center"
         onMouseOver={() => handleMouseOver(index)}
         onMouseLeave={() => setHoveredLink("")}
         onClick={() => handleClickLink(index)}>
             <div
-                className={` ease-in-out duration-1000 h-2 w-2 rounded-full mt-3 -mb-1 ${hoveredLink === index ? "bg-purple-600" : ""}  ${activeLink === index ? "bg-orange-400" : ""} `}
+                className={` ease-in-out duration-1000 h-2 w-2 rounded-full mt-3 xxs:max-sm:mt-1 xxs:max-sm:mb-1 xxs:max-sm:mt-0 -mb-1 ${hoveredLink === index ? "bg-purple-600" : ""}  ${activeLink === index ? "bg-orange-400" : ""} `}
             >
             </div>
-          <div className="h-8 py-3 mb-3 text-slate-400 hover:font-medium">{link}</div>
+          <div className="h-8 py-3 mb-3 xxs:max-sm:mb-1 text-slate-400 hover:text-fuchsia-300 active:text-sky-200 ease-in-out duration-700 hover:ease-out ">{link}</div>
            
         </div>
       ))}
